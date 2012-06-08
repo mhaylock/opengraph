4334af4767102466bcee8a68627c58e6282289d8
Mark Haylock <mark@resolvedigital.co.nz>
2012-06-08 17:52:51 +1200

    Added support for custom namespaces other than 'og:'.

--- lib/opengraph.rb
+++ lib/opengraph.rb
@@ -14,20 +14,22 @@ module OpenGraph
   rescue RestClient::Exception, SocketError
     false
   end
-  
+
   def self.parse(html, strict = true)
     doc = Nokogiri::HTML.parse(html)
     page = OpenGraph::Object.new
+    namespaces = doc.css('head').attribute('prefix').to_s.scan(/\s*(.+?): http[^#]+#/).flatten
     doc.css('meta').each do |m|
-      if m.attribute('property') && m.attribute('property').to_s.match(/^og:(.+)$/i)
-        page[$1.gsub('-','_')] = m.attribute('content').to_s
+      if m.attribute('property') && m.attribute('property').to_s.match(/^((?:#{namespaces.join('|')}):.+)$/i)
+        key = $1.gsub('-','_').gsub(/^og:/, '') # strip og: from og namespace properties
+        page[key] = m.attribute('content').to_s
       end
     end
     return false if page.keys.empty?
     return false unless page.valid? if strict
     page
   end
-  
+
   TYPES = {
     'activity' => %w(activity sport),
     'business' => %w(bar company cafe hotel restaurant),
@@ -38,38 +40,38 @@ module OpenGraph
     'product' => %w(album book drink food game movie product song tv_show),
     'website' => %w(blog website)
   }
-  
+
   # The OpenGraph::Object is a Hash with method accessors for
   # all detected Open Graph attributes.
   class Object < Hashie::Mash
     MANDATORY_ATTRIBUTES = %w(title type image url)
-    
+
     # The object type.
     def type
       self['type']
     end
-    
+
     # The schema under which this particular object lies. May be any of
     # the keys of the TYPES constant.
     def schema
-      OpenGraph::TYPES.each_pair do |schema, types| 
+      OpenGraph::TYPES.each_pair do |schema, types|
         return schema if types.include?(self.type)
       end
       nil
     end
-    
+
     OpenGraph::TYPES.values.flatten.each do |type|
       define_method "#{type}?" do
         self.type == type
       end
     end
-    
+
     OpenGraph::TYPES.keys.each do |scheme|
       define_method "#{scheme}?" do
         self.type == scheme || OpenGraph::TYPES[scheme].include?(self.type)
       end
     end
-    
+
     # If the Open Graph information for this object doesn't contain
     # the mandatory attributes, this will be <tt>false</tt>.
     def valid?
